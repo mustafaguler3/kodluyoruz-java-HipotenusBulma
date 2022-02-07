d8b30b3e2c1e756e694a2652915d5d8a76355296
mustafaguler3 <mustafa.glr@hotmail.com>
2022-02-07 15:28:18 +0300

    HipotenusBulma

--- HipotenusBulma/src/main.java
+++ HipotenusBulma/src/main.java
@@ -1,8 +1,29 @@
+import java.util.Scanner;
 
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		double edge1, edge2, edge3, u, area;
+
+
+	    Scanner input1 = new Scanner(System.in);
+	    System.out.print("Please write first edge : ");
+	    edge1 = input1.nextDouble();
+
+	    Scanner input2 = new Scanner(System.in);
+	    System.out.print("Please write second edge : ");
+	    edge2 = input2.nextDouble();
+
+	    Scanner input3 = new Scanner(System.in);
+	    System.out.print("Please write third edge : ");
+	    edge3 = input3.nextDouble();
+
+	    u = (edge1 + edge2 + edge3) / 2;
+	    area = Math.sqrt(u * (u - edge1) * (u - edge2) * (u - edge3));
+
+	    System.out.print("The area is : " + area);
 
 	}
 
